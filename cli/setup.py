c79b2d1eeb738e40975df35474233c28e0edf543
Younes Moustaghfir <younes@syntho.ai>
2024-02-14 14:35:57 +0100

    ci: add release pipeline that releases when a new tag is added

--- cli/setup.py
+++ cli/setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = "0.1.0"
+version = "{{VERSION_PLACEHOLDER}}"
 
 setup(
     name="syntho-cli",
